29c6ae97ee8161ef1d445d6ce160c4783f14926e
presto <yoohan95@gmail.com>
2018-11-16 11:25:30 +0900

    전체적 UI

---
+++ SchoolNoticeNotifier/BookmarkViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookmarkViewController.swift
+//  SchoolNoticeNotifier
+//
+//  Created by Presto on 15/11/2018.
+//  Copyright © 2018 presto. All rights reserved.
+//
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
