d20a29d9f4af264b0475bbbf620daebc699fdb26
presto <yoohan95@gmail.com>
2018-12-26 15:19:17 +0900

    University.swift 변경 수시로

--- UniTice/Model/University.swift
+++ UniTice/Model/University.swift
@@ -20,24 +20,32 @@ enum University: String, CaseIterable {
     // ㅂ
     // ㅅ
     case seoultech = "서울과학기술대학교"
+    case snu = "서울대학교"
     // ㅇ
     // ㅈ
+    case jnu = "전남대학교"
     // ㅊ
     // ㅋ
     // ㅌ
     // ㅍ
     // ㅎ
-    
+}
+
+extension University {
     static func generateModel() -> UniversityModel {
         let user = User.fetch() ?? User()
         let university = University(rawValue: user.university) ?? .dongduk
         switch university {
         case .dongduk:
-            return Dongduk()
+            return 동덕여자대학교()
         case .mju:
-            return Mju()
+            return 명지대학교()
+        case .snu:
+            return 서울대학교()
         case .seoultech:
-            return Seoultech()
+            return 서울과학기술대학교()
+        case .jnu:
+            return 전남대학교()
         }
     }
 }
