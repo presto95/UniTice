33ac6af76a935da7cef323de8e8a675e8e997294
presto <yoohan95@gmail.com>
2018-12-31 23:23:19 +0900

    로컬 알림 추가 코드 함수로 분리

--- UniTice/Main/Controller/MainContainerViewController.swift
+++ UniTice/Main/Controller/MainContainerViewController.swift
@@ -21,6 +21,53 @@ class MainContainerViewController: ButtonBarPagerTabStripViewController {
     override func viewDidLoad() {
         setupButtonBar()
         super.viewDidLoad()
+        registerLocalNotification()
+        setupUniversityLabel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        universityModel = University.generateModel()
+        reloadPagerTabStripView()
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        var viewControllers = [UITableViewController]()
+        for index in universityModel.categories.indices {
+            let contentViewController = MainContentTableViewController()
+            contentViewController.categoryIndex = index
+            contentViewController.universityModel = universityModel
+            viewControllers.append(contentViewController)
+        }
+        return viewControllers
+    }
+    
+    @IBAction private func touchUpSearchButton(_ sender: UIBarButtonItem) {
+        guard let next = UIViewController.instantiate(from: "Main", identifier: SearchViewController.classNameToString) as? SearchViewController else { return }
+        next.category = (viewControllers(for: self)[currentIndex] as? MainContentTableViewController)?.category
+        navigationController?.pushViewController(next, animated: true)
+    }
+}
+
+extension MainContainerViewController {
+    private func setupUniversityLabel() {
+        let universityLabel = UILabel()
+        universityLabel.text = universityModel.name
+        universityLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        universityLabel.sizeToFit()
+        let leftBarButtonItem = UIBarButtonItem(customView: universityLabel)
+        navigationItem.setLeftBarButton(leftBarButtonItem, animated: false)
+    }
+    
+    private func setupButtonBar() {
+        settings.style.selectedBarHeight = 5
+        settings.style.selectedBarBackgroundColor = .purple
+        settings.style.buttonBarBackgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        settings.style.buttonBarItemBackgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemFont = UIFont.systemFont(ofSize: 15, weight: .semibold)
+    }
+    
+    private func registerLocalNotification() {
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { isGranted, error in
             if let error = error {
@@ -47,46 +94,5 @@ class MainContainerViewController: ButtonBarPagerTabStripViewController {
             }
             UserDefaults.standard.set(true, forKey: "showsAlertIfPermissionDenied")
         }
-        setupUniversityLabel()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        universityModel = University.generateModel()
-        reloadPagerTabStripView()
-    }
-    
-    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
-        var viewControllers = [UITableViewController]()
-        for index in universityModel.categories.indices {
-            let contentViewController = MainContentTableViewController()
-            contentViewController.categoryIndex = index
-            contentViewController.universityModel = universityModel
-            viewControllers.append(contentViewController)
-        }
-        return viewControllers
-    }
-    
-    @IBAction private func touchUpSearchButton(_ sender: UIBarButtonItem) {
-        guard let next = UIViewController.instantiate(from: "Main", identifier: SearchViewController.classNameToString) as? SearchViewController else { return }
-        next.category = (viewControllers(for: self)[currentIndex] as? MainContentTableViewController)?.category
-        navigationController?.pushViewController(next, animated: true)
-    }
-    
-    private func setupUniversityLabel() {
-        let universityLabel = UILabel()
-        universityLabel.text = universityModel.name
-        universityLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        universityLabel.sizeToFit()
-        let leftBarButtonItem = UIBarButtonItem(customView: universityLabel)
-        navigationItem.setLeftBarButton(leftBarButtonItem, animated: false)
-    }
-    
-    private func setupButtonBar() {
-        settings.style.selectedBarHeight = 5
-        settings.style.selectedBarBackgroundColor = .purple
-        settings.style.buttonBarBackgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
-        settings.style.buttonBarItemBackgroundColor = #colorLiteral(red: 0.968627451, green: 0.968627451, blue: 0.968627451, alpha: 1)
-        settings.style.buttonBarItemTitleColor = .black
-        settings.style.buttonBarItemFont = UIFont.systemFont(ofSize: 15, weight: .semibold)
     }
 }
