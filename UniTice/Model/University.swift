89bdb0444bcc769ccba3372dd3f02633a56475f3
presto <yoohan95@gmail.com>
2018-12-31 14:25:05 +0900

    동국대학교 모델

--- UniTice/Model/University.swift
+++ UniTice/Model/University.swift
@@ -18,6 +18,7 @@ enum University: String, CaseIterable {
     case korea = "고려대학교"
     // ㄴ
     // ㄷ
+    case dongguk = "동국대학교"
     case dongduk = "동덕여자대학교"
     // ㄹ
     // ㅁ
@@ -57,6 +58,8 @@ extension University {
             return 경희대학교()
         case .korea:
             return 고려대학교()
+        case .dongguk:
+            return 동국대학교()
         case .dongduk:
             return 동덕여자대학교()
         case .mju:
